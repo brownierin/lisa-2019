72d0dd6faf23df1010c06e64d8f8a5767aa75db3
Erin Browning <ebrowning@slack-corp.com>
2019-10-28 15:18:33 -0700

    Add testing materials

---
+++ server/xmlhttprequest.js
@@ -0,0 +1,9 @@
+
+
+var url = "http://127.0.0.1:9000"
+var req = new XMLHttpRequest();
+req.addEventListener("load", reqListener);
+req.open("POST", url);
+req.setRequestHeader("Content-Type", "text\/plain")
+req.send();
+
